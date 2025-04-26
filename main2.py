990315cb20775910d9a9b419a7e7e4ab368420f0
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2025-04-26 20:45:33 +0300

    cfg

--- main2.py
+++ main2.py
@@ -16,16 +16,16 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 origins = [
-    "http://localhost:4200",
-    "http://127.0.0.1:4200"
+    "http://localhost:4200",  # Your local Angular app
+    "https://sbert-microservice.onrender.com",  # Your deployed Render app
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,  # Allow requests from both origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 
 try:
